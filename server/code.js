fc7d6107803c6da3f5d797f6a3e7d8bfcaedfcde
Squall410 <Squall410@aol.com>
2014-02-08 20:51:05 -0600

    Start of question/answer functionality.
    Server now handles multiple users to connect/disconnect and parses the
    most rudimentary question responses.
    Future plans include a list of responses that have recently occurred as
    well as better q/a parsing.

--- server/code.js
+++ server/code.js
@@ -1,19 +1,149 @@
-var chatBase = {};
+DEBUG = true;
 
-chatBase.stuff = function () {
-	console.log("do things!");
+if (DEBUG !== true) {
+	console = {};
+	console.log = function(){};
+	console.dir = function(){};
+}
+
+var chatBase = {
+	socket: null
+};
+chatBase.connect = function() {
+	// Get the name the user entered
+	var nameField = $("#name_input :input[name='username']");
+	chatBase.username = nameField.val();
+	console.dir(chatBase.username);
+	
+	// If we have a blank user name, then give an error.
+	if (chatBase.username === null || chatBase.username === '') {
+		$("#message_text").html("You cannot enter a blank user name. Please try again.");
+		return;
+	}
+	
+	// Make sure to clean up any socket we might still have around
+	if ( (typeof chatBase.socket !== "undefined") && (chatBase.socket !== null) ) {
+		delete chatBase.socket;
+	}
+	
+	// Stop access to this until we finish processing
+	nameField.prop("disabled", true);
+	nameField.val("");
+	
+	// Otherwise, log into the system
+	chatBase.socket = io.connect('http://localhost:8335', {'force new connection': true} );
+	chatBase.socket.on('connect', chatBase.onConnect);
+	// These are for handling connection error cases.
+	chatBase.socket.on('connect_failed', chatBase.handleError);
+	chatBase.socket.on('disconnect', chatBase.handleError);
+	chatBase.socket.on('error', chatBase.handleError);
+};
+
+chatBase.onConnect = function() {
+	// If we connect, stop listening for it
+	chatBase.socket.removeListener('connect', chatBase.onConnect);
+	
+	// Setup the functional listeners
+	chatBase.socket.on('welcome', chatBase.onWelcome);
+	chatBase.socket.on('userUpdate', chatBase.onUserUpdate);
+	chatBase.socket.on('receivedAnswer', chatBase.onReceivedAnswer);
+	chatBase.socket.on('newQuestion', chatBase.onNewQuestion);
+	chatBase.socket.on('answerFound', chatBase.onAnswerFound);
+	
+	// Tell the server our name
+	chatBase.socket.emit('newuser', { name: chatBase.username });
+	chatBase.username = null;
+};
+
+chatBase.onWelcome = function(data) {
+	// data:Object
+	// -- user:Object = The user you just created.
+	console.log("onWelcome: " + data);
+	
+	// Switch to a 'chatroom'
 	$("#welcome").css("display", "none");
 	$("#chatroom").css("display", "block");
 	
-	// Get the name the user entered
-	var username = $("#name_input :input[name='username']").val();
-	console.dir(username);
-	var socket = io.connect('http://localhost:8335');
-	socket.emit('newuser', { name: username });
-	socket.on('welcome', function (data) {
-		console.log(data);
-		$("#display_name").html(data.value);
-	});
+	// Clear any previous input
+	$("#q_input :input[name='answer']").val("");
+	
+	// Update the user's name
+	$("#display_name").html( "User: " + data.user.name );
+};
+
+chatBase.onUserUpdate = function(data) {
+	// data:Object
+	// -- userlist:String = HTML of connected users
+	console.log("onUserUpdate: " + data);
+	$("#user_list").html( "Users:<br/>" + data.userlist );
+};
+
+chatBase.onReceivedAnswer = function(data) {
+	// Re-enable answering if the server says they heard us.
+	$("#q_input :input[name='answer']").prop("disabled", false);
+};
+
+chatBase.onNewQuestion = function(data) {
+	// { name: currentQ.name, q: currentQ.q }
+	$("#question_header").html( "Question: " + data.name );
+	$("#question").html( data.q );
+	
+	var answerField = $("#q_input :input[name='answer']");
+	answerField.prop("disabled", false);
+	answerField.val("");
+};
+
+chatBase.submitAnswer = function() {
+	console.log( "Submitting an answer." );
+	
+	var answerField = $("#q_input :input[name='answer']");
+	var attempt = answerField.val();
+	answerField.val("");
+	
+	// If we have a blank answer, don't bother telling the server
+	if (attempt === null || attempt === '') {
+		return;
+	}
+	
+	answerField.prop("disabled", true);
+	chatBase.socket.emit('submitAnswer', { answer: attempt });
+};
+
+chatBase.onAnswerFound = function(data) {
+	// data:Object
+	//	- answer:String = the correct answer
+	//	- name:String = the user name of the person who answered correctly
+	//	- value:int = the value of the question they just answered
+	var answerField = $("#q_input :input[name='answer']");
+	answerField.prop("disabled", true);
+	answerField.val("");
+	$("#correct_user").html( "Correct answer of " + data.answer + " given by " + data.name + " for " + data.value + " points!" );
+	$("#question_header").html( "Question: " );
+	$("#question").html( "Loading..." );
+};
+
+chatBase.handleError = function() {
+	console.log( "Got an error" );
+	chatBase.socket.removeListener('connect', chatBase.onConnect);
+	chatBase.socket.removeListener('welcome', chatBase.onWelcome);
+	chatBase.socket.removeListener('userUpdate', chatBase.onUserUpdate);
+	chatBase.socket.removeListener('receivedAnswer', chatBase.onReceivedAnswer);
+	chatBase.socket.removeListener('newQuestion', chatBase.onNewQuestion);
+	chatBase.socket.removeListener('answerFound', chatBase.onAnswerFound);
+	chatBase.socket.removeListener('connect_failed', chatBase.handleError);
+	chatBase.socket.removeListener('disconnect', chatBase.handleError);
+	chatBase.socket.removeListener('error', chatBase.handleError);
+	
+	chatBase.socket.disconnect();
+	delete chatBase.socket;
+	chatBase.socket = null;
+	$("#welcome").css("display", "block");
+	$("#chatroom").css("display", "none");
+	
+	var nameField = $("#name_input :input[name='username']");
+	nameField.prop("disabled", false);
+	
+	$("#message_text").html("Connection error. Please reload or try again.");
 };
 
 window.onload = function() {
--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
-var app = require('http').createServer(handler);	// The http server
-var io = require('socket.io').listen(app);			// The socket.io reference
-var path = require('path');							// Helps with file paths
-var fs = require('fs');								// The filesystem reference
+var app = require('http').createServer(httpHandler);	// The http server
+var io = require('socket.io').listen(app);				// The socket.io reference
+var path = require('path');								// Helps with file paths
+var fs = require('fs');									// The filesystem reference
+var questionFile = require('./questions.json');
 
 app.listen(8335);
 
-function handler (req, res) {
+DEBUG = true;
+
+if (DEBUG !== true) {
+	console = {};
+	console.log = function(){};
+	console.dir = function(){};
+	io.enable('browser client minification');  // Send minified client
+	io.enable('browser client etag');          // Apply etag caching logic based on version number
+	io.set('log level', 1);
+}
+
+function httpHandler(req, res) {
 	var fileName = path.basename(req.url); //the file that was requested
 	console.log( "filename: " + fileName );
 	fs.exists(fileName, function (exists) {
@@ -28,12 +40,177 @@ function handler (req, res) {
 			};
 		});
 	});
-}
+};
 
-io.sockets.on('connection', function (socket) {
-	socket.on('newuser', function (data) {
-		console.log('newuser');
-		console.log(data);
-		socket.emit('welcome', { value: data.name });
+// THIS FUNCTION CREATES EVERYTHING THAT WE DO!
+var serverStart = function() {
+	// Prime the question system
+	getNewQuestion();
+	
+	// NOTE:: (JCW) Set the SocketIO listeners to know when a user connects
+	// 			This fires for 'reconnect' events as well.
+	// TODO:: HANDLE RECONNECT EVENTS (have a timeout on disconnect before you remove a user.)
+	io.sockets.on('connection', function (socket) {
+		socket.on('newuser', function (data) {
+			console.log('newuser');
+			console.log(data);
+			
+			var userData = createNewUser(data);
+			socket.on('requestUpdate', function() {
+				var htmlList = askForUserUpdate();
+				socket.emit("userUpdate", { userlist: htmlList });
+				
+			});
+			socket.on('disconnect', function () {
+				// TODO:: Use a timeout before removal. If the user connects again in that time, keep them alive.
+				removeUser(userData);
+			});
+			socket.on('submitAnswer', function(data) {
+				// data:Object
+				//	- answer:String = The answer the user is trying to compare
+				console.log( data );
+				
+				socket.emit('receivedAnswer', {});
+				if (currentQ != null && fetchingAnswer === false) {
+					processAnswerAttempt( data.answer, userData );
+				}
+				
+			});
+			socket.emit('welcome', { user: userData });
+			// If we have a question, send the Q part to the user
+			if (currentQ != null) {
+				socket.emit("newQuestion", { name: currentQ.name, q: currentQ.q });
+			}
+		});
 	});
-});
+};
+
+var userList = {};
+var userCount = 0;
+var questionList = [];
+var fetchingAnswer = true;
+var currentQ = null;
+
+// BEGIN:: QUESTION FUNCTIONS
+var getNewQuestion = function() {
+	if ( (typeof questionFile === "undefined") || (questionFile === null) ) {
+		throw new Error( "No questions file exists! Problem with server!" );
+		return;
+	}
+	var qList = questionFile.list;
+	if( Object.prototype.toString.call( qList ) !== '[object Array]' ) {
+		throw new Error( "Questions file list does not exist! Problem with server!" );
+		return;
+	}
+	if ( qList.length <= 0 ) {
+		throw new Error( "Questions file list does not exist! Problem with server!" );
+		return;
+	}
+	
+	// If we have no question, grab one. If we alreay were on a question (and we just answered it) get the next one.
+	// TODO:: (JCW) Track a history of the last N questions and don't let us repeat a question in that time.
+	var qIndex = parseInt(Math.random() * qList.length);
+	currentQ = qList[qIndex];
+	
+	if (currentQ == null || currentQ.name == null || currentQ.q == null) {
+		throw new Error( "Malformed question! Problem with server!" );
+		return;
+	}
+	
+	io.sockets.emit("newQuestion", { name: currentQ.name, q: currentQ.q });
+	fetchingAnswer = false;
+};
+
+var processAnswerAttempt = function(answer, userData) {
+	// If the answer is correct, you must award points to the user that submitted it.
+	// Then refresh all users, invalidate the question they have, pick a new one, and then show it.
+	
+	// TODO:: Have the answer checking parse math operations and better check than a simple string compare
+	var foundSolution = false;
+	var iter = 0;
+	var count = currentQ.a.length;
+	for (iter = 0; iter < count; ++iter) {
+		if (String(answer) == currentQ.a[iter]) {
+			foundSolution = true;
+		}
+		
+		if (foundSolution === true) {
+			break;
+		}
+	}
+	
+	// Award the current player points if they answered correctly.
+	if (foundSolution === true) {
+		var addedPoints = (currentQ.val || 1);
+		userData.points += addedPoints;
+		// Let everyone know the game is over, and get a new question!
+		fetchingAnswer = true;
+		currentQ = null;
+		io.sockets.emit("answerFound", { name: userData.name, answer: answer, value: addedPoints });
+		updateUsers();
+		
+		// Wait 2 seconds, and then get a new question for the players
+		setTimeout(getNewQuestion, 2000);
+	}
+};
+// END:: QUESTION FUNCTIONS
+
+// BEGIN:: USER FUNCTIONS
+var createNewUser = function(inputData) {
+	// inputData:Object
+	// - name:String
+	console.log( "createNewUser: " + inputData.name );
+	// Create an empty user
+	var user = {
+		name: inputData.name,
+		points: 0
+	};
+	
+	// Verify if another user exists with that name.
+	// If so, append an '_' and search again.
+	// BB:: Could potentially make a really long insertion.
+	// Maybe change to random integer appending? Real user creation?
+	while( userList[user.name] != null ) {
+		console.log( "check: " + userList[user.name] );
+		user.name = (user.name + "_");
+	}
+	// Finally, place the user into the userList
+	userList[user.name] = user;
+	++userCount;
+	console.log( "Created user with name: " + user.name );
+	
+	updateUsers();
+	return user;
+};
+
+var removeUser = function(user) {
+	if( userList[user.name] !== null ) {
+		console.log( "Removed user: " + user.name );
+		delete userList[user.name];
+		--userCount;
+		updateUsers();
+		return;
+	}
+	throw new Error( "Failed to remove the user: " + user.name );
+};
+
+var askForUserUpdate = function() {
+	var htmlList = '';
+	var key = null;
+	var tempUser = null;
+	for ( key in userList ) {
+		tempUser = userList[key];
+		htmlList += tempUser.name + ' <small>(' + tempUser.points + ' points)</small><br/>';
+	}
+	return htmlList;
+};
+
+var updateUsers = function() {
+	console.log( "updateUsers count: " + userCount );
+	var htmlList = askForUserUpdate();
+	io.sockets.emit("userUpdate", { userlist: htmlList });
+};
+// END:: USER FUNCTIONS
+
+// MUST CALL THIS TO START THE WHOLE SYSTEM
+serverStart();
